3f97d7bea2f9c4ae6d4e0ea7bb46faf128ea75bb
mustyilmaz <mustafa_yilmazz@outlook.com>
2024-04-21 02:29:01 +0300

    minor changes

--- server/routes/PhotoRoutes.js
+++ server/routes/PhotoRoutes.js
@@ -3,10 +3,17 @@ const router = express.Router();
 const multer = require('multer');
 const path = require('path');
 const Photo = require('../models/PhotoModel');
+const fs = require('fs');
+
+const storagePath = 'uploads/photos';
+
+if (!fs.existsSync(storagePath)) {
+  fs.mkdirSync(storagePath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'uploads/photos');
+    cb(null, storagePath);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
